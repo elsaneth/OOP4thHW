2eba632e7f823a92047886bb4977ff533fd4691b
elsaneth <elisabeth.suits@gmail.com>
2024-04-03 19:57:11 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,6 +18,25 @@ public class Main {
             }
         }
         System.out.println(Arrays.toString(words.toArray()));
+
+        Map<Character, Integer> letters = new HashMap<>();
+        List<Character> chars = new ArrayList<>(Arrays.asList('k', 'p', 't', 'r', 's'));
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                for (char c : chars) {
+                    if (Character.toLowerCase(word.charAt(i)) == c) {
+                        letters.put(c, letters.getOrDefault(c, 0) + 1);
+                    }
+                }
+            }
+        }
+
+        Map<Character, Integer> sortedLetters = letters
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println(sortedLetters);
     }
 
     public static Scanner findInputFile(String path) {
