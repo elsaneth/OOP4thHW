a4c5ff53a5cdafcf38cf15e19461ad3e63135121
elsaneth <elisabeth.suits@gmail.com>
2024-04-04 16:16:55 +0300

    task4

--- src/SuitsKodutoo4.java
+++ src/SuitsKodutoo4.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class SuitsKodutoo4 {
     public static void main(String[] args) {
-        String filePath = "/Users/elisabeth/Coding/OOP/OOP4thHW/src/sind.txt";
+        String filePath = "/Users/elisabeth/Coding/OOP/OOP4thHW/src/sisend.txt";
         Scanner scanner = findInputFile(filePath);
 
         List<String> words = new ArrayList<>();
@@ -29,6 +29,39 @@ public class SuitsKodutoo4 {
             List<String> sortedWordsRandS = getWordsStartingWithRorS(words);
             Collections.sort(sortedWordsRandS);
             System.out.println(sortedWordsRandS);
+
+            Map<String, Integer> wordsWithR = new HashMap<>();
+            Map<String, Integer> wordsWithS = new HashMap<>();
+            List<Character> firstLetters = Arrays.asList('r', 's');
+
+            for (String word : words) {
+                for (int i = 0; i < word.length(); i++) {
+                    char firstChar = Character.toLowerCase(word.charAt(0));
+                    if (firstLetters.contains(firstChar)) {
+                        if (firstChar == 's') {
+                            wordsWithS.put(word.toLowerCase(), wordsWithS.getOrDefault(word.toLowerCase(), 0) + 1);
+                        } else {
+                            wordsWithR.put(word.toLowerCase(), wordsWithR.getOrDefault(word.toLowerCase(), 0) + 1);
+                        }
+                    }
+                }
+            }
+
+            List<Map.Entry<String, Integer>> sortedEntriesR = wordsWithR.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(5)
+                    .toList();
+
+            List<Map.Entry<String, Integer>> sortedEntriesS = wordsWithS.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(5)
+                    .toList();
+
+            System.out.println(sortedEntriesR);
+            System.out.println(sortedEntriesS);
+
             scanner.close();
         }
     }
